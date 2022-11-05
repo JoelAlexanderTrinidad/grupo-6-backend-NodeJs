460118e77fdc29a59013bcba13d73de7dc66790f
yamil351 <leokasper@live.com.ar>
2022-11-05 11:03:06 -0300

    transactions post endpoint

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,24 +1,53 @@
-const { catchAsync } = require("../helpers/catchAsync")
-const createHttpError = require('http-errors')
-const { Transaction } = require('../database/models')
-const { endpointResponse } = require("../helpers/success")
-
+const { catchAsync } = require("../helpers/catchAsync");
+const createHttpError = require("http-errors");
+const { Transaction } = require("../database/models");
+const { endpointResponse } = require("../helpers/success");
+const {ErrorObject}= require("../helpers/error")
 
 module.exports = {
-    get: catchAsync(async(req, res, next) => {
-        try {
-            const response = await Transaction.findAll()
-            endpointResponse({
-                res,
-                message: 'Transactions retrieved successfully',
-                body: response
-            })
-        } catch (error) {
-            const httpError = createHttpError(
-                error.statusCode,
-            `[Error retrieving transactions]- [index- GET]:${error.message}`, 
-            )
-            next(httpError)
-        }
-    })
-}
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const response = await Transaction.findAll();
+      endpointResponse({
+        res,
+        message: "Transactions retrieved successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving transactions]- [index- GET]:${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+  post: catchAsync(async (req, res, next) => {
+    try {
+      if (
+        req.body.userId &&
+        req.body.categoryId &&
+        req.body.amount 
+      ) {
+        const newTransaction = new Transaction({
+         userId: req.body.userId,
+         categoryId: req.body.categoryId,
+         amount: req.body.amount
+        });
+      const savedTransaction = await newTransaction.save();
+      endpointResponse({
+        res,
+        message: "Transactions retrieved successfully",
+        body: savedTransaction,
+      });
+      }else{
+     {throw new ErrorObject("fields could not be validated", 400);}
+      }
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving transactions]- [index- POST]:${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { get } = require('../controllers/transactions')
+const { get,post } = require('../controllers/transactions')
 
 const router = express.Router()
 
 router.get('/', get)
+router.post('/', post)
 
 module.exports = router
