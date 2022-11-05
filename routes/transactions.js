406813a1dc94ad1889ff18fe3c9423708df0f637
yamil351 <leokasper@live.com.ar>
2022-11-05 12:55:22 -0300

    adding routes to delete transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -4,6 +4,7 @@ const {
   getTransaction,
   getTransactionUser,
   post,
+  deleteTransaction,
 } = require("../controllers/transactions");
 
 const router = express.Router()
@@ -12,6 +13,7 @@ const router = express.Router()
 router
     .get('/?', get)
     .get('/:id', getTransaction)
-    router.post("/", post);
+    .post("/", post)
+    .delete("/:id",deleteTransaction)
 
 module.exports = router
