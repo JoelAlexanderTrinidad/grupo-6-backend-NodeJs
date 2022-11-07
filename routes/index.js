22b3268e8d76deb4f699bfe92eea6b06c94b3211
harem123 <cp1085275@gmail.com>
2022-11-07 09:50:20 -0500

    minor fixes

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,14 @@
 const express = require('express')
 
 const usersRouter = require('./users')
-<<<<<<< HEAD
-const categoriesRouter = require('./categories')
-=======
-
 const categoriesRouter = require('./categories')
 const transactionsRouter = require('./transactions')
->>>>>>> 2767a78e3e06223076a45aebd540905e7e94d74d
 const authRouter = require('./auth')
-
-const transactionsRouter = require('./transactions')
-
-<<<<<<< HEAD
-// example of a route with index controller get function
-router.use('/users', usersRouter)
-=======
 
 const router = express.Router();
 
 router.use('/users', usersRouter)
 router.use('/transactions', transactionsRouter)
-
->>>>>>> 2767a78e3e06223076a45aebd540905e7e94d74d
 router.use('/categories', categoriesRouter)
 router.use('/auth', authRouter)
 
