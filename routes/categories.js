a95cdb79da0b7a2d17591aeb99892dc056e81d40
yamil351 <leokasper@live.com.ar>
2022-11-10 16:54:06 -0300

    swagger fix search and delete by id

--- routes/categories.js
+++ routes/categories.js
@@ -49,61 +49,73 @@ const router = express.Router();
 
 */
 /**
- * @swagger
- * /categories/{id}:
- *   delete:
- *     summary: Retrieve from controllers
- *     parameters:
- *       - in: query
- *         name: mode
- *         schema:
- *           type: string
- *         description: The mode of a workout
- *     responses:
- *       200:
- *         description: OK
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: string
- *                   example: OK
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *
- */
+ /
+* @swagger
+* /categories/{id}:
+*  get:
+*    summary: Find transaction by ID
+*    tags: [categories]
+*    parameters:
+*       - name: id
+*         in: path
+*         description: ID of categories to return
+*         required: true
+*         schema:
+*           type: integer
+*           format: int64
+*    responses:
+*        '200':
+*          description: successfuly operation
+*          content:
+*            application/json:
+*              schema:
+*                $ref: '#/components/schemas/categories'
+*            application/xml:
+*              schema:
+*                $ref: '#/components/schemas/categories'
+*        '400':
+*          description: Invalid ID supplied
+*        '404':
+*          description: categories not found
+*        '500':
+*          description: error of server
+*    security:
+*     - ApiKeyAuth: []
+*/
 /**
- * @swagger
- * /categories/{id}:
- *   delete:
- *     summary: Retrieve from controllers
- *     parameters:
- *       - in: query
- *         name: mode
- *         schema:
- *           type: string
- *         description: The mode of a workout
- *     responses:
- *       200:
- *         description: OK
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: string
- *                   example: OK
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *
- */
+ /
+* @swagger
+* /categories/{id}:
+*  delete:
+*    summary: Find categories by ID
+*    tags: [categories]
+*    parameters:
+*       - name: id
+*         in: path
+*         description: ID of categories to return
+*         required: true
+*         schema:
+*           type: integer
+*           format: int64
+*    responses:
+*        '200':
+*          description: successfuly operation
+*          content:
+*            application/json:
+*              schema:
+*                $ref: '#/components/schemas/categories'
+*            application/xml:
+*              schema:
+*                $ref: '#/components/schemas/categories'
+*        '400':
+*          description: Invalid ID supplied
+*        '404':
+*          description: categories not found
+*        '500':
+*          description: error of server
+*    security:
+*     - ApiKeyAuth: []
+*/
 router.get("/", getCategories);
 
 router.delete("/:id", deleteCategory);
