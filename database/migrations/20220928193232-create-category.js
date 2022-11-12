ed76264dff859e20bd7a9d638c25c4a73463538f
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-11-12 17:26:50 -0500

    update categories migration

--- database/migrations/20220928193232-create-category.js
+++ database/migrations/20220928193232-create-category.js
@@ -14,9 +14,7 @@ module.exports = {
       deletedAt: {
         type: Sequelize.STRING
       },
-      deletedAt: {
-        type: Sequelize.STRING
-      },
+     ,
       description: {
         type: Sequelize.STRING,
       },
