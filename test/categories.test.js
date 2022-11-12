86715f157fe4239b1e41cc29359a6b29abb07247
Sasha Rodriguez Carlomagno <caballerosasha@hotmail.com>
2022-11-12 11:22:34 -0300

    categories tests finished

--- test/categories.test.js
+++ test/categories.test.js
@@ -30,6 +30,55 @@ describe('Categories endpoints (CRUD) tests', () => {
         expect(allCategories.body.body.length).to.eql(1);
     })
 
+    it('should show an error if the new category format is wrong (POST /categories)', async () => {
+        const category = {
+            nombre: "prueba",
+            description: "test category"
+        }
+
+        let response = await myReq.post('/categories').send(category);
+
+        expect(response.statusCode).to.be.equal(500);
+    })
+
+    it('should update a created product (PUT /categories/:id)', async () => {
+        const newCategory = {
+            name: "prueba",
+            description: "TEST category"
+        }
+
+        let allCategories = await myReq.get('/categories');
+
+        let categoryId = allCategories.body.body[0].id;
+
+        let response = await myReq.put(`/categories/${categoryId}`).send(newCategory);
+
+        let modifiedCategory = response.body.body;
+
+        expect(modifiedCategory[0]).to.eql(1);
+        expect(response.body.code).to.be.equal(200);
+    })
+
+    it('should show an error if update product has a wrong format (PUT /categories/:id)', async () => {
+        const newCategory = {
+            nombre: "prueba",
+            description: "TEST category"
+        }
+
+        let allCategories = await myReq.get('/categories');
+
+        let categoryId = allCategories.body.body[0].id;
+
+        let response = await myReq.put(`/categories/${categoryId}`).send(newCategory);
+
+        let modifiedCategory = response.body.body;
+
+        expect(modifiedCategory[0]).to.eql(1);
+
+
+
+    })
+
     it('should delete category with given id (DELETE /categories/:id)', async function() {
         let allCategories = await myReq.get('/categories');
         let categoryId = allCategories.body.body[0].id;
@@ -39,6 +88,15 @@ describe('Categories endpoints (CRUD) tests', () => {
 
         expect(response.body.code).to.eql(200); 
         expect(allCategories.body.body.length).to.eql(0);
+    })
+
+    it('should show an error if given id category does not exist (DELETE /categories/:id)', async () => {
+        let categoryId = 214215124125;
+
+        let response = await myReq.delete(`/categories/${categoryId}`);
+
+        expect(response.statusCode).to.eql(404);
+        expect(response.body.code).to.eql(undefined); 
     })
 
     before(function() {
