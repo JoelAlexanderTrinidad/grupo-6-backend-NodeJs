021dda06f4968faf77fa61517105671a0b04d0e2
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-11-13 18:47:02 -0500

    Revert "add transactions.js (#4)" (#46)
    This reverts commit af69a0b86d8e468b14bbecfe587d34495e00a1de.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
-const transactionsRouter = require('./routes/transactions')
 
 const port = process.env.PORT || 3000
 
@@ -21,7 +20,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/transactions', transactionsRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
